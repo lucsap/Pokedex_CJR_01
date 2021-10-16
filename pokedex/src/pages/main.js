09dd999c23b9646ea2a0fdfb5b43b800aa3ed598
Lucas Pereira <lucaspework@gmail.com>
2021-10-16 13:49:09 -0300

    feat: title funcionando

--- pokedex/src/pages/main.js
+++ pokedex/src/pages/main.js
@@ -1,7 +1,15 @@
+import {Helmet} from "react-helmet";
+
 export default function Main() {
 
     return (
         <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Pokédex</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+                <meta name="description" content="teste de title para a main" />
+            </Helmet>
             <h1>Ideias</h1>
             <p>Como começamos nossa página principal?</p>
         </div>
