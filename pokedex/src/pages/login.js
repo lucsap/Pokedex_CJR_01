eef0e54e01d30a79d98b61e5e77f7ca0ebfb13ae
Lucas Pereira <lucaspework@gmail.com>
2021-10-16 02:02:35 -0300

    feat: antes do react-router-dom

---
+++ pokedex/src/pages/login.js
@@ -0,0 +1,14 @@
+export default function Login() {
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <labe>Insira seu nome de usuário</labe>
+                <input></input>
+                <br/>
+                <button>Fazer login</button>
+            </form>
+        </div>
+    )
+}
