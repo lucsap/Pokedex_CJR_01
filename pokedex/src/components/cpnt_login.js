31033b1f912aa5af63d285e71a179fa2f51093c0
Lucas Pereira <lucaspework@gmail.com>
2021-10-23 13:43:11 -0300

    Update cpnt_login.js

--- pokedex/src/components/cpnt_login.js
+++ pokedex/src/components/cpnt_login.js
@@ -1,5 +1,5 @@
-const Navbar = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+  color: 
 `;
