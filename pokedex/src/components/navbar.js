cc0545bd6da7c5e32016ba3784357b7bd676cfc6
Lucas Pereira <lucaspework@gmail.com>
2021-10-24 17:10:31 -0300

    feat: react-icons

--- pokedex/src/components/navbar.js
+++ pokedex/src/components/navbar.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { CgPokemon, CgProfile } from 'react-icons/cg';
+import { IoExitOutline,  IoHomeOutline} from "react-icons/io5";
 
 export const Menu = styled.header`
     text-align: right;
@@ -11,6 +13,7 @@ export const Menu = styled.header`
 export const A = styled.a`
     display: inline-block;
     color: blue;
+    font-size: 1.3rem;
     text-decoration: none;
     text-align: right;
     margin: 1rem;
