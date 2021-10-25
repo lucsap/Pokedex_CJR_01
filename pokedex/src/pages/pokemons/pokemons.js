54247d1922b0081604f4fcc6395eb332c6bb6f81
gustavodezan <63751057+gustavodezan@users.noreply.github.com>
2021-10-24 23:20:28 -0300

    Update pokemons.js

--- pokedex/src/pages/pokemons/pokemons.js
+++ pokedex/src/pages/pokemons/pokemons.js
@@ -1,13 +1,33 @@
 import Titles from '../../components/helmet'
 import Navbar from '../../components/navbar'
+import Pokemon from '../../components/Pokemon'
+import api from '../../resources/api';
 
-export default function Pokemons() {
+import { useState,useEffect } from 'react'
+
+export default function Pokemons(props) {
+    const [fvpkm, setFvpkm] = useState([])
+    const user = localStorage.getItem('user'); // tem o usuário logado
+
+    function getFavPokemons(){
+        api.get("https://pokedex20201.herokuapp.com/users/"+user)
+        .then((resp)=>{
+          console.log("Debuglist",resp)
+          setFvpkm(resp.data.pokemons)
+          console.log("Pokelist:",fvpkm)
+        })// eslint-disable-next-line
+    }
+    
+    useEffect(()=>{
+      getFavPokemons()
+    },[])
 
     return (
-        <div>
+        <div className="App">
             <Titles title={"Pokémons favoritos"} />
             <Navbar />
-            <h1>Favoritos</h1>
+            <h1>Seus Pokemons</h1>
+            <Pokemon pk={fvpkm}/>
         </div>
     )
 }
