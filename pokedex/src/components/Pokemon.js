e24cd1e03eb8bb9767da756519ea701b653a7aa2
gustavodezan <63751057+gustavodezan@users.noreply.github.com>
2021-10-24 23:19:24 -0300

    Update Pokemon.js

--- pokedex/src/components/Pokemon.js
+++ pokedex/src/components/Pokemon.js
@@ -1,7 +1,9 @@
 import { useState } from "react"
 import styled from "styled-components"
+import { CgPokemon } from 'react-icons/cg';
+import api from "../resources/api";
 
-const Li = styled.li`
+const Li = styled.button`
     width: 180px;
     margin-left: auto;
     margin-right: 15px;
@@ -12,6 +14,9 @@ const Li = styled.li`
     background-image: linear-gradient(-45deg,${props => props.pkid==props.pkidhover ? (`${props.kindcol[props.kindp[0]]},${(props.kindp.length==2 ? props.kindcol[props.kindp[1]] : props.kindcol[props.kindp[0]])}`): `${"#2d2d2d"},${"#2d2d2d"}`});
     list-style-type: none;
     display: inline-block;
+    border-color: #8d7272;
+    border-style: ${props => `${(props.pkid==props.pkidhover ? "none ": "dashed")}`};
+    border-width: 0.05cm;
 `
 
 const Line = styled.line`
@@ -26,6 +31,11 @@ const Line = styled.line`
   padding-bottom: 4px;
   background: ${props => `${props.pkid==props.pkidhover ? null : props.bg}`};
   list-style-type: none;
+`
+
+const Button = styled.button`
+  background: rgba(255, 165, 0, 0.00);
+  border-color: rgba(255, 165, 0, 0.00);
 `
 
 function Pokemon(props){
@@ -52,6 +62,8 @@ function Pokemon(props){
 
 const [radius, setRadius] = useState(30);
 const [pokeId,setPokeId] = useState(0);
+const user = localStorage.getItem('user'); // tem o usuário logado
+const [fvpkm, setFvpkm] = useState([])
 
 function Alerta(){
       console.log("ALERTA")
@@ -73,23 +85,60 @@ function Alerta(){
       }
     }
 
-    // reestruturar html
+    // reestruturar html - llIIIIlllIIlIlIlllIlIIl
+
+    function getFavPokemons(){
+      api.get("https://pokedex20201.herokuapp.com/users/"+user)
+      .then((resp)=>{
+        console.log("Debuglist",resp)
+        setFvpkm(resp.data.pokemons)
+        console.log("Pokelist:",fvpkm)
+      })// eslint-disable-next-line
+    }
+
+    function unfav(name){
+      api.delete("https://pokedex20201.herokuapp.com/users/"+props.us+"/starred/"+name)
+        .then((resp)=>{
+            window.alert('Você deixou seu '+name+' ir embora...')
+        }).catch((error)=>(console.log()))
+      }
+
+    function favorite(name) {
+      // check for pokemon
+      getFavPokemons()
+      fvpkm.map(
+        (favor)=>{
+          if (name==favor.name){
+            return unfav(name)
+          }
+        }
+      )
+      console.log(name)
+      api.post("https://pokedex20201.herokuapp.com/users/"+props.us+"/starred/"+name)
+        .then((resp)=>{
+            window.alert('Você capturou um '+name+' selvagem')
+        }).catch((error)=>(console.log()))
+    }
 
     return (
       <>
         {props.pk.map(
           (pkm)=>
             <Li
-            onClick={Alerta} onMouseEnter={() => setPokeId(pkm.id)} onMouseLeave={() => setPokeId(0)} 
+            onMouseEnter={() => setPokeId(pkm.id)} onMouseLeave={() => setPokeId(0)} 
             pkidhover={pokeId} pkid={pkm.id} rd={radius} kindcol={type_color}
             kindp={pkm.kind.split(";")}
             >
+            <Button onClick={()=>favorite(pkm.name)}>
+              <CgPokemon size="17px" color={"white"} title="favoritar"/>
+            </Button>
             {pkm.name}
             <br/>
             <img src={pkm.image_url}/>
             <br/>
             <li>{splitKind(pkm)}</li>
           </Li>
+          
         )}
       </>
     )
