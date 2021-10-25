0dfdddb375ab2a391a8d9c371be43dedbb95a227
gustavodezan <63751057+gustavodezan@users.noreply.github.com>
2021-10-25 01:27:42 -0300

    Update navbar.js

--- pokedex/src/components/navbar.js
+++ pokedex/src/components/navbar.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components'
 import { CgPokemon, CgProfile } from 'react-icons/cg';
 import { IoExitOutline,  IoHomeOutline} from "react-icons/io5";
+import { useHistory } from "react-router-dom"
+import { useState } from 'react';
 
 export const Menu = styled.header`
     text-align: right;
     text-decoration: none;
     background-color: rgb(255,0,68);
-    border-radius: 1rem;
+    #border-radius: 1rem;
     border: 1px solid white;
     margin: 1rem;
     display: inline-block;
@@ -33,15 +35,27 @@ export const Exit = styled.div`
     display: inline-block;
     margin-top: 1rem;
 `
-
-
+export const NavDiv = styled.h1`
+    font-size: 1.6rem;
+    font-color: black;
+`
+    
 function Navbar(props) {
+    const [user, setUsers] = useState("")
+    const [value, setValue] = useState("")
+    let history = useHistory()
+    function unlog(){
+        setUsers(null)
+        localStorage.setItem('user', value);
+        history.push("/main")
+    }
+
     return(
         <Menu>
             <ul>
                 <A href="../main"><IoHomeOutline/> Início</A>
                 <A href="../pokemons"><CgProfile/> Perfil</A>
-                <A href="../login"><Exit>
+                <A onClick={unlog} href="../login"><Exit>
                     <IoExitOutline />
                 </Exit> Logar/Sair</A>
             </ul>
