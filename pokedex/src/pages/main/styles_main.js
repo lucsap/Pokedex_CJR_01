75b717ba4aa2ab996dc5c87cd942f9211d90c362
gustavodezan <63751057+gustavodezan@users.noreply.github.com>
2021-10-24 23:59:33 -0300

    Update styles_main.js

---
+++ pokedex/src/pages/main/styles_main.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const PageInput = styled.div`
+  background-color: rgb(255,0,68);
+  color: black;
+  padding: 10px;
+  border: 2px solid black;
+  border-radius: 10px;
+  display: inline-block;
+  margin: 5px;
+  cursor: pointer;
+  text-decoration: black;
+  &:hover {
+    background-color: #ffbf00;
+  }
+`;
