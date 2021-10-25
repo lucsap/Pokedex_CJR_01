1fe077905b67a5bb2dd1a1b274901b5a01e30fd8
gustavodezan <63751057+gustavodezan@users.noreply.github.com>
2021-10-24 23:57:08 -0300

    Update styles_login.js

--- pokedex/src/pages/login/styles_login.js
+++ pokedex/src/pages/login/styles_login.js
@@ -1,10 +1,25 @@
 import styled from 'styled-components'
 
-export const LoginStyles = styled.h1`
-  color: blue;
-  background-color: yellow;
+export const LoginStyles = styled.div`
+text-size: 200px;
+  color: white;
   text-align: center;
   align-content: center;
+`;
+
+export const LoginButton = styled.div`
+  background-color: rgb(255,0,68);
+  color: black;
+  padding: 10px;
+  border: 2px solid black;
+  border-radius: 10px;
+  display: inline-block;
+  margin: 5px;
+  cursor: pointer;
+  text-decoration: black;
+  &:hover {
+    background-color: #ffbf00;
+  }
 `;
 
 export const Contents = styled.div`
